1952758990c66e1c63962283e9d756deb8329cfc
Noodz2Code <chunkiatng70@gmail.com>
2024-01-06 22:06:18 +0800

    structure changed, with class and method, add recording

--- cam.py
+++ cam.py
@@ -2,36 +2,67 @@ import cv2
 import time
 import datetime
 
-#set on which camera to record using cv2.VideoCapture() method
-camera = cv2.VideoCapture(0)
+#main camera
+class OpenCamera():
+    #set on which camera to record using cv2.VideoCapture() method
+    camera = cv2.VideoCapture(0)
 
-#add detection to face and body
-face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-body_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
+    #add detection to face and body
+    face_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+    body_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_fullbody.xml")
 
-while True:
-    _, frame = camera.read()    #read the frame and display
 
-    #change frame color to gray
-    disp_color = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+class DetectionCam(OpenCamera):
 
-    #to detect face and body, set detectection speed with detectMultiScale() method
-    faces = face_detector.detectMultiScale(disp_color, 1.1, 5)
-    body = body_detector.detectMultiScale(disp_color, 1.2, 5)
+    #check frame size for recorded video
+    frame_size = (int(OpenCamera().camera.get(3)), int(OpenCamera().camera.get(4)))
 
-    #draw a rectangle near the object if detected
-    #for face
-    for (x, y, width, height) in faces:
-        cv2.rectangle(frame, (x,y), (x+width, y+height), (255,0,0), 3)
+    #save recording as mp4
+    save_record = cv2.VideoWriter_fourcc(*"mp4v")
 
-    #for body
-    for (x, y, width, height) in body:
-        cv2.rectangle(frame, (x,y), (x+width, y+height), (255,0,0), 3)
+    output = cv2.VideoWriter("sample_video.mp4", save_record, 20.0, frame_size, True)
 
-    cv2.imshow("Camera", frame)
+    while True:
+        _, frame = OpenCamera().camera.read()    #read the frame and display
 
-    #way to quit the program, which ord('q') shows which key to press to quit
-    if cv2.waitKey(1) == ord('q'):
-        break
+        #change frame color to gray
+        disp_color = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-camera.release() 
+        #to detect face and body, set detectection speed with detectMultiScale() method
+        faces = OpenCamera().face_detector.detectMultiScale(disp_color, 1.1, 5)
+        body = OpenCamera().body_detector.detectMultiScale(disp_color, 1.2, 5)    
+        
+        output.write(frame)
+
+        output.release()
+
+        #draw a rectangle near the object if detected
+        #for face
+        for (x, y, width, height) in faces:
+            cv2.rectangle(frame, (x,y), (x+width, y+height), (255,0,0), 3)
+
+        #for body
+        for (x, y, width, height) in body:
+            cv2.rectangle(frame, (x,y), (x+width, y+height), (255,0,0), 3)
+
+        cv2.imshow("Camera", frame)
+
+        #way to quit the program, which ord('q') shows which key to press to quit
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+    OpenCamera().camera.release() 
+    cv2.destroyAllWindows()
+
+#to record
+class recordFrame(DetectionCam):
+    #start recording
+    record = True
+
+#main
+def main():
+    OpenCamera()
+    DetectionCam()
+    recordFrame()
+
+main()
